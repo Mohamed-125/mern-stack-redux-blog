6e9da9327dd88f3dd8f594159ba9ed22c95ace80
Mohamed-125 <mohamedibrahim.fed@gmail.com>
2024-02-16 17:09:49 +0200

    f commit

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -6,10 +6,10 @@ import {
 import axios from "axios";
 
 const base =
-  import.meta.env.MODE === "development"
+  import.meta.env.VITE_MODE === "development"
     ? "http://localhost:3000/"
     : "https://mern-stack-redux-blog.onrender.com/";
-console.log(import.meta.env.MODE, "");
+console.log(import.meta.env, "");
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: base || "",
